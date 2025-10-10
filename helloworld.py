a7aa32f298a3042c1637b3247342229c9a739da5
Arunkasi <dommetiarunkasi@gmail.com>
2025-10-10 22:15:34 +0530

    My first certificate website version

---
+++ helloworld.py
@@ -0,0 +1,47 @@
+#print("Arunkasi")
+#print(10 , 20 , 30 ,sep = '\n')
+#print(10 , 20 , 30 , sep = 'Arun')
+#print(10 , 20 , 30 , end = " Arunkasi")
+#print(end="\n")
+#a = int(input((' enter a number:')))
+#print(a+ 10)
+
+#a = int(input('enter a number') )
+#b = int(input('enter a number'))
+#print(a+b)
+#print(end="\n")
+#a = 100
+#b = 200
+#c = a+b
+#print("sum of {} and {} is {}".format(a,b,c))
+#print(f"sum of {a} and {b} is {c}")
+
+#side = 10
+#area = side * side
+#perimeter = 4 * side 
+#print(f"Area of square with side value  {side} is  {area} and perimeter is  {perimeter}")
+#print("Area of square with side value  {} is  {} and perimeter is  {}".format(side,area,perimeter))
+
+#n = int(input('enter number to get multiples upto 20:'))
+#for i in range (1,20):
+ #   print(f'{n} x {i} = {n*i}')
+ 
+#a, b = map(int, input().split())
+#print(a +b)
+
+#x = 2.68787
+#print(x)
+#print(  f'{x : .2f}')
+#name = 'Arun'
+#print(name)
+#name_Arun = 'Reshu'
+#print(name_Arun)
+#print(len(name))
+#print(len(name_Arun))
+#print(name[0])
+#print(name[-2])
+#print(name[-3])
+#print(name[0:1])
+#print(name[1:])
+
+
